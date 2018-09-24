3692e7bf5a430303f01616c6e513d9ee91076eff
suusanex <suusan@himado.net>
2018-09-25 00:07:01 +0900

    UDP追加試し（未動作確認）

---
+++ CSTcpProxy/CSUdpProxy/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CSUdpProxy
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e?.Exception.ToString());
+        }
+    }
+}
---
+++ CSTcpProxy/CSUdpProxy/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSUdpProxy
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void TraceLog(string msg)
+        {
+            System.Diagnostics.Trace.WriteLine("CSUDPProxy:" + msg);
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() => RunSv());
+        }
+        async void RunSv()
+        {
+            TraceLog("Start");
+
+            var udpRecieve = new UdpClient(new IPEndPoint(IPAddress.Loopback, 50080));
+            var udpSend = new UdpClient("10.0.0.12", 50080);
+        
+            var firstRecieveRet = await udpRecieve.ReceiveAsync();
+
+            await udpSend.SendAsync(firstRecieveRet.Buffer, firstRecieveRet.Buffer.Length);
+
+            var taskForward = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var recieveRet = await udpRecieve.ReceiveAsync();
+
+                    await udpSend.SendAsync(recieveRet.Buffer, recieveRet.Buffer.Length);
+                }
+            });
+
+            var taskReverse = Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var sendReciveRet = await udpSend.ReceiveAsync();
+
+                    await udpRecieve.SendAsync(sendReciveRet.Buffer, sendReciveRet.Buffer.Length,
+                        firstRecieveRet.RemoteEndPoint);
+                }
+            });
+
+            
+            Task.WaitAll(taskForward, taskReverse);
+        }
+    }
+}
